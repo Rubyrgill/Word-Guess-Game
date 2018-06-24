82d198878b3e9fc516f73d8a92f1f397d3fff2d1
RubyG <rubygillr@gmail.com>
2018-06-24 12:01:57 -0400

    start game function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,3 +12,29 @@ var wins = 0;
 var losses = 0;
 var guessesRemaining = 9;
 
+//FUNCTIONS
+
+
+
+function Game() {
+    //computer generates random word from words array
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    // split the individual word into separate arrays, and store in new array 
+    lettersOfWord = randomWord.split("");
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length;
+
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    //HTML
+    document.getElementById("currentword").innerHTML = "GUESS THIS WORD:     " + blanksAndCorrect.join("  ");
+
+    //console logging 
+    console.log(randomWord);
+    console.log(lettersOfWord)
+    console.log(blanks)
+    console.log(blanksAndCorrect)
+}
