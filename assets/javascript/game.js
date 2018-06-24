bd578b5ec42a154a74708e489378cab9e225c8d2
RubyG <rubygillr@gmail.com>
2018-06-24 14:27:40 -0400

    Javascript includes, audio play, and image chance when answer correct

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 
 //VARIABLES
 
-var words = ["arthur", "rugrats", "thesimpsons", "scoobydoo", "spongebob", "cyberchase", "teentitans", "dannyphantom"]
+var words = ["arthur", "rugrats", "thesimpsons", "scoobydoo", "spongebob", "dannyphantom", "teentitans"]
 
 var randomWord = "";
 var lettersOfWord = []
@@ -47,22 +47,85 @@ function Game() {
 var a = document.getElementById("arthur");
 var r = document.getElementById("rugrats");
 var simpsons = document.getElementById("simpsons");
+var scoobydoo = document.getElementById("scoobydoo");
+var spongebob = document.getElementById("spongebob");
+var danny = document.getElementById("danny");
+var teent = document.getElementById("teent");
 
 // function for audio 
 function aud() {
     if (randomWord === words[0]) {
+        scoobydoo.pause();
+        spongebob.pause();
+        danny.pause();
+        teent.pause();
+        simpsons.pause();
         r.pause();
         a.play();
         document.getElementById("image").src = "https://yt3.ggpht.com/a-/ACSszfGqnF8nezfhGPz8bfUScK5EPj-AifAMVwzU5g=s900-mo-c-c0xffffffff-rj-k-no";
-    } else if (randomWord === words[1]) {
+    }
+    else if (randomWord === words[1]) {
+        scoobydoo.pause();
+        spongebob.pause();
+        danny.pause();
+        teent.pause();
+        simpsons.pause();
         a.pause();
         r.play();
+        document.getElementById("image").src = "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Series/103873/103873._SX360_QL80_TTD_.jpg";
     }
     else if (randomWord === words[2]) {
+        scoobydoo.pause();
+        spongebob.pause();
+        danny.pause();
+        teent.pause();
+        r.pause();
         a.pause();
-        r.pause()
         simpsons.play();
+        document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Simpsons_FamilyPicture.png/220px-Simpsons_FamilyPicture.png";
     }
+    else if (randomWord === words[3]) {
+        spongebob.pause();
+        danny.pause();
+        teent.pause();
+        simpsons.pause();
+        r.pause();
+        a.pause();
+        scoobydoo.play();
+        document.getElementById("image").src = "https://is3-ssl.mzstatic.com/image/thumb/Purple118/v4/5a/69/b8/5a69b811-ffaf-b1f6-bcc0-ea72a9091ac8/AppIcon-1x_U007emarketing-85-220-5.png/246x0w.jpg";
+    }
+    else if (randomWord === words[4]) {
+        danny.pause();
+        teent.pause();
+        simpsons.pause();
+        r.pause();
+        a.pause();
+        scoobydoo.pause();
+        spongebob.play();
+        document.getElementById("image").src = "https://pbs.twimg.com/profile_images/1002272769352978433/9S4QWSR0.jpg";
+    }
+    else if (randomWord === words[5]) {
+        spongebob.pause();
+        teent.pause();
+        simpsons.pause();
+        r.pause();
+        a.pause();
+        scoobydoo.pause();
+        danny.play();
+        document.getElementById("image").src = "https://i.ytimg.com/vi/gfxsLHecG5E/maxresdefault.jpg";
+    }
+    else if (randomWord === words[6]) {
+        spongebob.pause();
+        danny.pause();
+        simpsons.pause();
+        r.pause();
+        a.pause();
+        scoobydoo.pause();
+        teent.play();
+        document.getElementById("image").src = "https://nerdist.com/wp-content/uploads/2018/06/Teen_Titans_Homemade_Wallpaper_1920x1080_2.jpg";
+    }
+
+
 };
 
 function reset() {
