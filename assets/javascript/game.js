93fa961602ad97283cbe2c3fa5c6c1d8c70201d6
RubyG <rubygillr@gmail.com>
2018-06-24 14:52:11 -0400

    Cleaned code, and updated images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,35 @@
 
 //VARIABLES
-
 var words = ["arthur", "rugrats", "thesimpsons", "scoobydoo", "spongebob", "dannyphantom", "teentitans"]
 
+//Empty variables to store values later
 var randomWord = "";
 var lettersOfWord = []
 var blanks = 0;
 var blanksAndCorrect = [];
 var wrongGuess = [];
+
+//Counter Variables
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 9;
 
-//FUNCTIONS
 
 
+// ALL FUNCTIONS
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+
+//__________________________________________________________
+//GAME START FUNCTION
+//__________________________________________________________
 function Game() {
     //computer generates random word from words array
     randomWord = words[Math.floor(Math.random() * words.length)];
 
     // split the individual word into separate arrays, and store in new array 
     lettersOfWord = randomWord.split("");
+
     //store length of word in blanks, for later use
     blanks = lettersOfWord.length;
 
@@ -30,7 +38,7 @@ function Game() {
         blanksAndCorrect.push("_");
     }
 
-    //HTML
+    //showing the "_" within HTML
     document.getElementById("currentword").innerHTML = "GUESS THIS WORD:     " + blanksAndCorrect.join("  ");
 
     //console logging 
@@ -40,10 +48,12 @@ function Game() {
     console.log(blanksAndCorrect)
 }
 
-//AUDIO 
-//-----------------------------------
 
-//variables for audio 
+//__________________________________________________________
+//AUDIO FUNCTION
+//__________________________________________________________
+
+//variables for audio function
 var a = document.getElementById("arthur");
 var r = document.getElementById("rugrats");
 var simpsons = document.getElementById("simpsons");
@@ -52,8 +62,10 @@ var spongebob = document.getElementById("spongebob");
 var danny = document.getElementById("danny");
 var teent = document.getElementById("teent");
 
-// function for audio 
+
 function aud() {
+    //Arthur Audio & Image
+    //---------------------------
     if (randomWord === words[0]) {
         scoobydoo.pause();
         spongebob.pause();
@@ -64,6 +76,8 @@ function aud() {
         a.play();
         document.getElementById("image").src = "https://yt3.ggpht.com/a-/ACSszfGqnF8nezfhGPz8bfUScK5EPj-AifAMVwzU5g=s900-mo-c-c0xffffffff-rj-k-no";
     }
+    //Rugrats Audio & Image
+    //---------------------------
     else if (randomWord === words[1]) {
         scoobydoo.pause();
         spongebob.pause();
@@ -74,6 +88,8 @@ function aud() {
         r.play();
         document.getElementById("image").src = "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Series/103873/103873._SX360_QL80_TTD_.jpg";
     }
+    //Simpsons Audio & Image
+    //---------------------------
     else if (randomWord === words[2]) {
         scoobydoo.pause();
         spongebob.pause();
@@ -84,6 +100,8 @@ function aud() {
         simpsons.play();
         document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Simpsons_FamilyPicture.png/220px-Simpsons_FamilyPicture.png";
     }
+    //Scooby-doo Audio & Image
+    //---------------------------
     else if (randomWord === words[3]) {
         spongebob.pause();
         danny.pause();
@@ -94,6 +112,8 @@ function aud() {
         scoobydoo.play();
         document.getElementById("image").src = "https://is3-ssl.mzstatic.com/image/thumb/Purple118/v4/5a/69/b8/5a69b811-ffaf-b1f6-bcc0-ea72a9091ac8/AppIcon-1x_U007emarketing-85-220-5.png/246x0w.jpg";
     }
+    //Spongebob Audio & Image
+    //---------------------------
     else if (randomWord === words[4]) {
         danny.pause();
         teent.pause();
@@ -104,6 +124,8 @@ function aud() {
         spongebob.play();
         document.getElementById("image").src = "https://pbs.twimg.com/profile_images/1002272769352978433/9S4QWSR0.jpg";
     }
+    //Danny Audio & Image
+    //---------------------------
     else if (randomWord === words[5]) {
         spongebob.pause();
         teent.pause();
@@ -114,6 +136,8 @@ function aud() {
         danny.play();
         document.getElementById("image").src = "https://i.ytimg.com/vi/gfxsLHecG5E/maxresdefault.jpg";
     }
+    //Teen Titans Audio & Image
+    //---------------------------
     else if (randomWord === words[6]) {
         spongebob.pause();
         danny.pause();
@@ -124,16 +148,21 @@ function aud() {
         teent.play();
         document.getElementById("image").src = "https://nerdist.com/wp-content/uploads/2018/06/Teen_Titans_Homemade_Wallpaper_1920x1080_2.jpg";
     }
-
-
 };
 
+//__________________________________________________________
+//RESET FUNCTION
+//__________________________________________________________
 function reset() {
     guessesRemaining = 9;
     wrongGuess = [];
     blanksAndCorrect = [];
     Game()
 }
+
+//__________________________________________________________
+//CHECK LETTERS/COMPARE FUNCTION
+//__________________________________________________________
 
 //If/Else, to see if letter selected matches random word
 function checkLetters(letter) {
@@ -158,47 +187,58 @@ function checkLetters(letter) {
         wrongGuess.push(letter);
         guessesRemaining--;
     }
-
     console.log(blanksAndCorrect);
 }
 
+//__________________________________________________________
+//FINAL COMPLETE FUNCTION
+//__________________________________________________________
 
+//check to see if player won...
 function complete() {
     console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
 
-    //check to see win 
+    //if WON...then alert, play audio, display image and reset new round
     if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
         wins++;
         alert("YOU WON!")
         aud()
         reset()
-
+        //display wins on screen
         document.getElementById("winstracker").innerHTML = "WINS:  " + wins;
 
+        //if LOST...then alert and reset new round
     } else if (guessesRemaining === 0) {
         losses++;
         alert("YOU LOSE")
         reset()
         document.getElementById("losstracker").innerHTML = "LOSSES:  " + losses;
     }
-    //check to see if lost
+    //display losses on screen && guesses remaining countdown
     document.getElementById("currentword").innerHTML = "GUESS THIS WORD:     " + blanksAndCorrect.join(" ");
     document.getElementById("guessesremaining").innerHTML = "GUESSES REMAINING:  " + guessesRemaining;
 }
 
 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//_____________________________________________________
+// EXECUTE CODE 
+//_____________________________________________________
 
-// PROCESS
-
-
+//call start game function
 Game()
-// //
+
+//check for keyup, and convert to lowercase then store in guesses
 document.onkeyup = function (event) {
     var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+    //check to see if guess entered matches value of random word
     checkLetters(guesses);
+    //process wins/loss 
     complete();
+    //store player guess in console for reference 
     console.log(guesses);
 
+    //display/store incorrect letters on screen
     document.getElementById("playerguesses").innerHTML = "LETTERS YOU GUESSED:   " + wrongGuess.join(" ");
 }
 
