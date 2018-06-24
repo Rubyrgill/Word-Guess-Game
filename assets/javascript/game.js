1a60e500680fa513439422b3aa44f1c2ab85a25d
RubyG <rubygillr@gmail.com>
2018-06-24 12:00:07 -0400

    Updated html, and added variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+
+//VARIABLES
+
+var words = ["arthur", "rugrats", "thesimpsons", "scoobydoo", "recess", "spongebob", "cyberchase", "franklin"]
+
+var randomWord = "";
+var lettersOfWord = []
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongGuess = [];
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 9;
+
